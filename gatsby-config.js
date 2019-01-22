cacebdd27124ac98577fce60097802c60ab099e7
Jasper Fawkes <design@honeymkr.com>
2019-01-22 09:09:19 -0500

    edited google analytics plugin options

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,27 +32,14 @@ module.exports = {
       },
     },
     
-    
+
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-104685851-1",
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        //exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "www.honeymkr.com",
       },
     },
+
     'gatsby-plugin-sass',
     'gatsby-plugin-offline'
   ],
